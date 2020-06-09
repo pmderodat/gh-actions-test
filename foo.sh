4e1a1108968f9321ddd36d7d8479d1d13554ce7b
Pierre-Marie de Rodat <derodat@adacore.com>
2020-06-09 10:39:11 +0200

    Install e3-core outside of the shell script

--- foo.sh
+++ foo.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
-trap 'echo "Interrupted" >&2 ; exit 1' INT
-
-# On Windows, python3/pip3 don't explicitly exist
-if [ "${OS:-}" == "Windows_NT" ]; then
-    run_python=python
-    run_pip=pip
-else
-    run_python=python3
-    run_pip=pip3
-fi
-
-echo Python version: $($run_python --version)
-echo Pip version: $($run_pip --version)
-
-$run_pip install --upgrade e3-testsuite
-echo Python search paths:
-$run_python -c "import sys; print('\n'.join(sys.path))"
-
 echo ==== file ====
 file 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows'
 echo ">>>" $?
