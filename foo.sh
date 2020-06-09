78e414b2faf86d01f9ab6ae180afa4d920682588
Pierre-Marie de Rodat <derodat@adacore.com>
2020-06-09 10:32:09 +0200

    Do not stop at the first error

--- foo.sh
+++ foo.sh
@@ -3,9 +3,6 @@
 trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR
 trap 'echo "Interrupted" >&2 ; exit 1' INT
 
-set -o errexit
-set -o nounset
-
 # On Windows, python3/pip3 don't explicitly exist
 if [ "${OS:-}" == "Windows_NT" ]; then
     run_python=python
@@ -24,11 +21,16 @@ $run_python -c "import sys; print('\n'.join(sys.path))"
 
 echo ==== 1 ====
 file 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows'
+echo ">>>" $?
 
 echo ==== 2 ====
 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows'
+echo ">>>" $?
 
 echo ==== 3 ====
 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows' 5 ls
+echo ">>>" $?
 
+echo ==== 4 ====
 $run_python test.py
+echo ">>>" $?
