998f928aa2f0dda41e79c3b87043fe4f0d3c3d04
Pierre-Marie de Rodat <derodat@adacore.com>
2020-06-09 10:35:54 +0200

    Rework rlimit tests

--- foo.sh
+++ foo.sh
@@ -19,18 +19,18 @@ $run_pip install --upgrade e3-testsuite
 echo Python search paths:
 $run_python -c "import sys; print('\n'.join(sys.path))"
 
-echo ==== 1 ====
+echo ==== file ====
 file 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows'
 echo ">>>" $?
 
-echo ==== 2 ====
+echo ==== rlimit: no args ====
 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows'
 echo ">>>" $?
 
-echo ==== 3 ====
+echo ==== rlimit: ls ====
 'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows' 5 ls
 echo ">>>" $?
 
-echo ==== 4 ====
-$run_python test.py
+echo ==== rlimit: python ====
+'C:\\hostedtoolcache\\windows\\Python\\3.8.3\\x64\\lib\\site-packages\\e3\\os\\data\\rlimit-x86-windows' 5 python -c'print("hello")'
 echo ">>>" $?
