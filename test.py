9d4b56b961133ee5247cfd564390a6151ef5a4fb
Pierre-Marie de Rodat <derodat@adacore.com>
2020-06-09 09:25:00 +0200

    Initial test

---
+++ test.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from e3.os.platform import get_rlimit
+import subprocess
+
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+rlimit = get_rlimit()
+argv = [rlimit, "5", sys.executable, "subp.py"]
+print("Argv:", argv)
+print("Cwd:", cwd)
+subprocess.run(argv, cwd=cwd)
